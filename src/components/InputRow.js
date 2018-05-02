f4bb4afcb5fc3a8e744b6f8ab059cea844bf9bcb
Marc Gartner <mogartner@gmail.com>
2018-05-01 22:38:55 -0400

    source links

--- src/components/InputRow.js
+++ src/components/InputRow.js
@@ -39,10 +39,10 @@ export default class InputRow extends Component {
                     }}
                     disabled={this.props.row.disabled}
                     defaultValue={this.props.row.body}
-                    autoCapitalize={false}
-                    autoComplete={false}
-                    autoCorrect={false}
-                    spellCheck={false}
+                    autoCapitalize="false"
+                    autoComplete="false"
+                    autoCorrect="false"
+                    spellCheck="false"
                 />
             </div>
         );
